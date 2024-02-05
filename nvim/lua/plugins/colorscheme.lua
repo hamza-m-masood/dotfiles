974451a0781011cae0f3e1bca9b7ccec38fda985
HamzaMasood1 <hamzamasood183@gmail.com>
2024-02-05 23:10:13 +0000

    updated dotfiles

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,35 @@
+-- return {
+--   -- add gruvbox
+--   { "ellisonleao/gruvbox.nvim" },
+--
+--   -- Configure LazyVim to load gruvbox
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       colorscheme = "gruvbox",
+--     },
+--   },
+-- }
+return {
+  {
+    "catppuccin/nvim",
+    lazy = false,
+    name = "catppuccin",
+    -- you can do it like this with a config function
+    config = function()
+      require("catppuccin").setup({
+        -- configurations
+      })
+    end,
+    -- or just use opts table
+    opts = {
+      -- configurations
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
---
+++ nvim/lua/plugins/disabled.lua
@@ -0,0 +1,5 @@
+return {
+  { "nvimdev/dashboard-nvim", enabled = false },
+  { "folke/flash.nvim", enabled = false },
+  { "stevearc/dressing.nvim", enabled = false },
+}
